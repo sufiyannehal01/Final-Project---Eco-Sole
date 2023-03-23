679c20ffcbb79babe894c9c50ecc094b78e24a84
sufiyannehal01 <nehalsufiyan@gmail.com>
2023-03-23 14:42:35 -0700

    Signup Functionality
    Successful!

--- Final Project Revised/RedStore Code/login.js
+++ Final Project Revised/RedStore Code/login.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-  import { getAuth,createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+  import { getAuth,signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,19 +25,21 @@
   var password =document.getElementById("login_password")
   var password_confirmation =document.getElementById("login_password_confirmation")
 
-  // making an fucntion for storing data 
-  window.signup = function(e){
+  window.login=function(e){
     e.preventDefault();
     var obj = {
         email:email.value,
         password:password.value,
         password_confirmation:password_confirmation.value
-    }
-    createUserWithEmailAndPassword(auth, obj.email,obj.password)
-    .then(function(success){ 
-        alert("Signup Successfully")
+    };
+    signInWithEmailAndPassword(auth,obj.email,obj.password,obj.password_confirmation)
+    .then(function(success){
+        console.log(user.uid)
+        alert("Sign In Successfully")
     })
-    .catch(function(err)
-    {alert("error" + err)})
-  console.log(obj)
-};
+    .catch(function(err){
+        window.location.replace('account.html')
+        alert("Sign In error" + err)
+    })
+    console.log(obj)
+  }
